d10ee6f97b1fe39f74ff0a8cf6e06c8389961778
varmelbita <142501542+varmelbita@users.noreply.github.com>
2024-06-19 13:41:54 +0100

    Update Architectures.blade.php
    Modifications
    - Suppression du menu
    j'ai juste mis la ligne 5 en commentaire pour que le menu ne  s'affiche plus.

--- backend/resources/views/content/Architectures/Architectures.blade.php
+++ backend/resources/views/content/Architectures/Architectures.blade.php
@@ -1,11 +1,15 @@
+
+
 @extends('layouts/contentLayoutMaster')
 
 @section('title', 'Architectures')
 
 @section('content')
     <!-- Page layout -->
-    <div id="app"></div>
+    <!-- <div id="app"></div> -->
     <!--/ Page layout -->
+
+    <!-- Suppression du menu j'ai juste mis la ligne  5 en commentaire    -->
 
     @vite("resources/views/content/Architectures/main.js")
 @endsection
