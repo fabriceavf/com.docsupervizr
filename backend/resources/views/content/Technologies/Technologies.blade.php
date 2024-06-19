4d73e5f503331812f13faa4ce0106cae35df70b6
varmelbita <142501542+varmelbita@users.noreply.github.com>
2024-06-19 14:11:19 +0100

    Update Technologies.blade.php
    Modification :
    Suppression du menu j'ai juste mis la ligne  5 en commentaire

--- backend/resources/views/content/Technologies/Technologies.blade.php
+++ backend/resources/views/content/Technologies/Technologies.blade.php
@@ -4,8 +4,11 @@
 
 @section('content')
     <!-- Page layout -->
-    <div id="app"></div>
+    <!-- <div id="app"></div> -->
     <!--/ Page layout -->
+
+    <!-- Suppression du menu j'ai juste mis la ligne  5 en commentaire    -->
 
     @vite("resources/views/content/Technologies/main.js")
 @endsection
+
