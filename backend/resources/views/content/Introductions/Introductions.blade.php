348d9ba335e73884b784a9d514edbccbc0480685
varmelbita <142501542+varmelbita@users.noreply.github.com>
2024-06-19 16:49:15 +0100

    Update Introductions.blade.php
    Modification :
    Suppressions des listes contenant  du texte et remplacer par un paragraphe

--- backend/resources/views/content/Introductions/Introductions.blade.php
+++ backend/resources/views/content/Introductions/Introductions.blade.php
@@ -5,15 +5,17 @@
 @section('content')
     <div class="container">
         <div>
-            <h3 id="cahier-des-charges-complet-gestion-des-absences-des-cong-s-des-postes-de-travail-des-horaires-g-n-ration-des-programmes-de-travail-syst-me-de-pointage-par-badge-myfare-validation-des-heures-suppl-mentaires-et-impression-des-badges"> Gestion des Absences, des Congés, des Postes de Travail, des Horaires, Génération des Programmes de Travail, Système de Pointage par Badge MYFARE, Validation des Heures Supplémentaires et Impression des Badges</h3>
-            <p><strong>1. Objectif Général :</strong></p>
+            <h3 id="cahier-des-charges-complet-gestion-des-absences-des-cong-s-des-postes-de-travail-des-horaires-g-n-ration-des-programmes-de-travail-syst-me-de-pointage-par-badge-myfare-validation-des-heures-suppl-mentaires-et-impression-des-badges">Presentation de Supervizr </h3>
+            <!-- <p><strong>1. Objectif Général :</strong></p>
             <ul>
                 <li>Développer une application permettant la gestion efficace des absences, des congés, des postes de travail, des horaires, la génération des programmes de travail, l&#39;intégration d&#39;un système de pointage par badge MYFARE, la validation des heures supplémentaires, ainsi que la gestion des agents et l&#39;impression des badges.</li>
             </ul>
             <p><strong>2. Description Générale :</strong></p>
-            <ul>
-                <li>L&#39;application doit fournir une plateforme centralisée pour la gestion des ressources humaines, incluant la gestion des absences, des congés, la définition et la gestion des postes de travail, la planification des horaires, la génération automatique des programmes de travail, l&#39;intégration avec un système de pointage par badge MYFARE pour enregistrer et vérifier la présence des agents, ainsi que la validation des heures supplémentaires en se basant sur les horaires de travail programmées pour chaque agent et les pointages collectés. De plus, elle doit permettre la gestion des informations personnelles et professionnelles des agents, ainsi que l&#39;impression de leurs badges.</li>
-            </ul>
+            <ul> -->
+
+            <p>L&#39;application doit fournir une plateforme centralisée pour la gestion des ressources humaines, incluant la gestion des absences, des congés, la définition et la gestion des postes de travail, la planification des horaires, la génération automatique des programmes de travail, l&#39;intégration avec un système de pointage par badge MYFARE pour enregistrer et vérifier la présence des agents, ainsi que la validation des heures supplémentaires en se basant sur les horaires de travail programmées pour chaque agent et les pointages collectés. De plus, elle doit permettre la gestion des informations personnelles et professionnelles des agents, ainsi que l&#39;impression de leurs badges.</p>
+               
+            
         </div>
     </div>
 @endsection
