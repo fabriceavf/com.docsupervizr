58e29e3f6e46c9fc9cd391bd0bb60492242c7439
varmelbita <142501542+varmelbita@users.noreply.github.com>
2024-06-25 13:52:28 +0100

    Create 2024_06_25_122103_create_propositions.php
    Modification des champs de la table "Proposition"

---
+++ backend/database/migrations/2024_06_25_122103_create_propositions.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    
+        try {
+            Schema::create('propositions', function (Blueprint $table) {
+                $table->id();
+                $table->string('taches_id');
+                $table->date('date')->nullable();
+                $table->string('proposition')->nullable();
+                $table->string('etats')->nullable();
+                $table->string('reponses')->nullable();
+                $table->date('debut')->nullable();
+                $table->date('fin')->nullable();
+                $table->string('creat_by')->nullable();
+                $table->schemalessAttributes('extra_attributes')->nullable();
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        } catch (\Throwable $e) {
+        
+        }
+    }
+        
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('propositions');
+    }
+};
+
+
