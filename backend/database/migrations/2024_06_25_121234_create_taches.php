af2b2e7ba1fcdc37560ef45f0a497c8a5b8bdc53
varmelbita <142501542+varmelbita@users.noreply.github.com>
2024-06-25 13:42:46 +0100

    Create 2024_06_25_121234_create_taches.php
    Modification des champs pour la table "Propositions"

---
+++ backend/database/migrations/2024_06_25_121234_create_taches.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+   
+        try{
+            Schema::create('taches', function (Blueprint $table) {
+                $table->id();
+                $table->string('libelle');
+                $table->date('deadline')->nullable();
+                $table->string('description')->nullable();
+                $table->string('creat_by')->nullable();
+                $table->schemalessAttributes('extra_attributes')->nullable();
+                $table->timestamps();
+                $table->softDeletes();
+            });
+         }catch (\Throwable $e){
+        
+            }
+        }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taches');
+    }
+};
+
+
+
